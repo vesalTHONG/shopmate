7bc1f82aae331176ff3b1b930c47cb217c58811b
vesalTHONG <2021093thong@aupp.edu.kh>
2022-11-22 18:01:34 +0700

    section 21

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('category/<slug:category_slug>/', views.store, name = 'products_by_category'),#how we set the path in the website
     path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'), #set path to product product_detail
     path('search/', views.search, name='search'), #path to the search function
+     path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'),
 ]
